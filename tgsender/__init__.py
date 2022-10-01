ed000a436464540ed770765f5e146c447cd1e4eb
apenasrr <apenasrr@gmail.com>
2022-10-01 11:21:21 -0300

    v0.1.1

--- tgsender/__init__.py
+++ tgsender/__init__.py
@@ -1,7 +1,7 @@
 """Top-level package for tgsender."""
-from .tgsender import main, get_data_upload_plan
 from . import api
+from .tgsender import get_data_upload_plan, main
 
 __author__ = """apenasrr"""
 __email__ = "apenasrr@gmail.com"
-__version__ = "0.1.0"
+__version__ = "0.1.1"
